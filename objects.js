e5dcef0978d1497aa6731b3e9c1f6ffb82c7956c
Milan Sacher <milan.lanlada@gmail.com>
2018-06-13 03:47:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,3 +9,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist["Phil Ochs"] = "Here's to the State of Mississippi" 
 }
 
+const PhilOchs = "Phil Ochs"
+
+var playlist = { Alison: "Slowdive", Sometimes: "My Bloody Valentine", PhilOchs: "Here's to the State of Mississippi" };
+
+delete playlist.Alison;
