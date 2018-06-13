6f988e0a3da7744d79c392aff4e4ff845379b20a
Milan Sacher <milan.lanlada@gmail.com>
2018-06-13 04:01:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,9 +9,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist["Phil Ochs"] = "Here's to the State of Mississippi";
 }
 
-function removeFromPlaylist(playlist, Alison) {
+function removeFromPlaylist(playlist, Slowdive) {
   
-  delete playlist.Alison;
+  delete playlist.Slowdive;
   
   return playlist
   
