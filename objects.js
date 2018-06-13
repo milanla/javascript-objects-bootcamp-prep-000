538a21dda34b747f10dba99ca7e6f1fe9d14040b
Milan Sacher <milan.lanlada@gmail.com>
2018-06-13 03:05:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,2 @@
 var playlist = { LDR: "White Mustang" }
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist['Slowdive'] = 'Alison';
-  playlist['My Bloody Valentine'] = 'Sometimes';
-  playlist["Here's to the State of Mississippi"] = 'Phil Ochs';
-  
-  return playlist
-}
