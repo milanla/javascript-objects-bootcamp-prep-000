7e890e7839671db3137503c84173e2023c3e9612
Milan Sacher <milan.lanlada@gmail.com>
2018-06-13 03:50:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,9 +3,13 @@ var playlist = { LDR: "White Mustang" }
 var playlist = {
   Alison: "Slowdive",
   Sometimes: "My Bloody Valentine"
-}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist["Phil Ochs"] = "Here's to the State of Mississippi" 
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Allison
+  
+  return playlist
+}
